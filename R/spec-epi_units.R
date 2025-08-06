15a02058becfe9d243b01ab6aba22c36b6c1a3d2
elidaniels <eli.daniels@ardata.fr>
2025-08-06 18:04:34 +0200

    fix: error when validating non-geospatial epi_units

--- R/spec-epi_units.R
+++ R/spec-epi_units.R
@@ -18,7 +18,8 @@
     validation_func = list(
       "are missing" = function(x) !sf::st_is_empty(x),
       "are not geospatial polygons" = function(x) {
-        sf::st_geometry_type(x) %in% c("POLYGON", "MULTIPOLYGON")
+        "sfc" %in% class(x) &&
+        all(sf::st_geometry_type(x) %in% c("POLYGON", "MULTIPOLYGON"))
         }
     )
   )
